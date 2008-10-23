add18734b9d273282fc139784d4eaa5563bdc68a
Jesse Grosjean <jesse@hogbaysoftware.com>
2008-10-23 13:33:09 -0400

    added kvo observer helper code

--- Blocks.h
+++ Blocks.h
@@ -15,6 +15,13 @@
 #define BLocalizedStringFromTableInBundle(key, tbl, bundle, comment) [bundle localizedStringForKey:(key) value:@"" table:(tbl)]
 #define BLocalizedStringWithDefaultValue(key, tbl, bundle, val, comment) [bundle localizedStringForKey:(key) value:(val) table:(tbl)]
 
+// derived from http://rentzsch.com/trac/wiki/JRSwizzle
 @interface NSObject (BlocksMethodSwizzle)
 + (BOOL)replaceMethod:(SEL)originalSelelector withMethod:(SEL)replacementSelector;
+@end
+
+// derived from http://www.mikeash.com/svn/BlocksKVONotificationCenter
+@interface NSObject (BlocksKVONotification)
+- (void)addObserver:(id)observer forKeyPath:(NSString *)keyPath selector:(SEL)selector userInfo:(id)userInfo options:(NSKeyValueObservingOptions)options;
+- (void)removeObserver:(id)observer keyPath:(NSString *)keyPath selector:(SEL)selector;
 @end
--- Blocks.m
+++ Blocks.m
@@ -33,3 +33,109 @@
 }
 
 @end
+
+// derived from http://www.mikeash.com/svn/MAKVONotificationCenter
+@interface BlocksKVONotificationHelper : NSObject {
+	id observer;
+	SEL selector;
+	id userInfo;
+	id target;
+	NSString *keyPath;
+}
+
+- (id)initWithObserver:(id)observer object:(id)target keyPath:(NSString *)keyPath selector:(SEL)selector userInfo:(id)userInfo options: (NSKeyValueObservingOptions)options;
+- (void)deregister;
+
+@end
+
+@implementation BlocksKVONotificationHelper
+
+static char BlocksKVONotificationHelperMagicContext;
+
+- (id)initWithObserver:(id)anObserver object:(id)aTarget keyPath:(NSString *)aKeyPath selector:(SEL)aSelector userInfo:(id)aUserInfo options:(NSKeyValueObservingOptions)options {
+	if(self = [self init]) {
+		observer = anObserver;
+		selector = aSelector;
+		userInfo = aUserInfo;		
+		target = aTarget;
+		keyPath = aKeyPath;		
+		[target addObserver:self forKeyPath:aKeyPath options:options context:&BlocksKVONotificationHelperMagicContext];
+	}
+	return self;
+}
+
+- (void) observeValueForKeyPath:(NSString *)aKeyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (context == &BlocksKVONotificationHelperMagicContext) {
+		// we only ever sign up for one notification per object, so if we got here
+		// then we *know* that the key path and object are what we want
+		((void (*)(id, SEL, NSString *, id, NSDictionary *, id))objc_msgSend)(observer, selector, aKeyPath, object, change, userInfo);
+	} else {
+		[super observeValueForKeyPath:aKeyPath ofObject:object change:change context:context];
+	}
+}
+
+- (void)deregister {
+	[target removeObserver:self forKeyPath:keyPath];
+}
+
+@end
+
+@interface BlocksKVONotificationCenter : NSObject {
+	NSMutableDictionary *observerHelpers;
+}
+@end
+
+@implementation BlocksKVONotificationCenter
+
++ (id)defaultCenter {
+	static BlocksKVONotificationCenter *defaultCenter = nil;
+	if (!defaultCenter) {
+		defaultCenter = [[self alloc] init];
+//		BlocksKVONotificationCenter *newCenter = [[self alloc] init];
+//		OSAtomicCompareAndSwapPtrBarrier(nil, newCenter, (void *)&defaultCenter);
+	}
+	return defaultCenter;
+}
+
+- (id)init {
+	if (self = [super init]) {
+		observerHelpers = [[NSMutableDictionary alloc] init];
+	}
+	return self;
+}
+
+- (id)dictionaryKeyForObserver:(id)observer object:(id)target keyPath:(NSString *)keyPath selector:(SEL)selector {
+	return [NSString stringWithFormat:@"%p:%p:%@:%p", observer, target, keyPath, selector];
+}
+
+- (void)addObserver:(id)observer object:(id)target keyPath:(NSString *)keyPath selector:(SEL)selector userInfo:(id)userInfo options: (NSKeyValueObservingOptions)options {
+	BlocksKVONotificationHelper *helper = [[BlocksKVONotificationHelper alloc] initWithObserver:observer object:target keyPath:keyPath selector:selector userInfo:userInfo options:options];
+	id key = [self dictionaryKeyForObserver:observer object:target keyPath:keyPath selector:selector];
+	@synchronized(self) {
+		[observerHelpers setObject:helper forKey:key];
+	}
+}
+
+- (void)removeObserver:(id)observer object:(id)target keyPath:(NSString *)keyPath selector:(SEL)selector {
+	id key = [self dictionaryKeyForObserver:observer object:target keyPath:keyPath selector:selector];
+	BlocksKVONotificationHelper *helper = nil;
+	@synchronized(self) {
+		helper = [observerHelpers objectForKey:key];
+		[observerHelpers removeObjectForKey:key];
+	}
+	[helper deregister];
+}
+
+@end
+
+@implementation NSObject (BlocksKVONotification)
+
+- (void)addObserver:(id)observer forKeyPath:(NSString *)keyPath selector:(SEL)selector userInfo:(id)userInfo options:(NSKeyValueObservingOptions)options {
+	[[BlocksKVONotificationCenter defaultCenter] addObserver:observer object:self keyPath:keyPath selector:selector userInfo:userInfo options:options];
+}
+
+- (void)removeObserver:(id)observer keyPath:(NSString *)keyPath selector:(SEL)selector {
+	[[BlocksKVONotificationCenter defaultCenter] removeObserver:observer object:self keyPath:keyPath selector:selector];
+}
+
+@end
